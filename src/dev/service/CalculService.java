da33d7b56810a627334672eabe580608035c6b95
Dorian <dorian.bonnamy@gmail.com>
2019-02-25 09:21:43 +0100

    first commit

---
+++ src/dev/service/CalculService.java
@@ -0,0 +1,10 @@
+package dev.service;
+public class CalculService {
+	public int additionner(String expression) {
+		// TODO Ajouter un log en niveau DEBUG affichant &quot;Evaluation de l&#39;expression &lt;expression&gt;&quot;.
+		// TODO par exemple &quot;Evaluation de l&#39;expression 1+4&quot;
+		
+		
+		return 0;
+	}
+}
